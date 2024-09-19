87697e1d96a07bf20747fec5d8fa89e084835c24
INDavid04 <davidirimia04@gmail.com>
2024-09-19 17:08:35 +0300

    add: l-01 second challenge

--- index.js
+++ index.js
@@ -8,18 +8,36 @@ const menu = [
 const cashInRegister = 100
 const orderQueue = []
 
-/**
- * Challenge: Add a utility function "addNewPizza" that takes a pizza object
- * and adds it to the menu.
- */
-
-// Me
-function addNewPizza() {
-    let pizzaObject = { name: "Ton", price: 12 }
-    menu.push(pizzaObject)
-}
-
-// Him
 function addNewPizza(pizzaObj) {
     menu.push(pizzaObj)
+}
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ * 
+ * Solution
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+ */
+
+function placeOrder(pizzaName) {
+    let newOrder
+    for (let i = 0; i < menu.length; i++) {
+        if (menu.name === pizzaName) {
+            cashInRegister += menu.price
+            newOrder = { pizza: pizzaName, status: "ordered" }
+            orderQueue.push(newOrder)
+        }
+    }
+    return newOrder
 }
