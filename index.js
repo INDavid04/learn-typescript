211474b8611b09a68cc1ef9bcd77cbc1f97eda08
INDavid04 <davidirimia04@gmail.com>
2024-09-19 16:58:56 +0300

    add: l-01 first challenge

---
+++ index.js
@@ -0,0 +1,25 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+
+// Me
+function addNewPizza() {
+    let pizzaObject = { name: "Ton", price: 12 }
+    menu.push(pizzaObject)
+}
+
+// Him
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
