10311772f6dd1e4a5e193d2de59a2b673a3c1cb0
INDavid04 <davidirimia04@gmail.com>
2024-09-20 10:01:22 +0300

    add: l-04 Obligatory types basics lesson

--- index.ts
+++ index.ts
@@ -1,39 +1,6 @@
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
-]
+let myName: string = "Bob"
+// Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
 
-let cashInRegister = 100
-let nextOrderId = 1
-const orderQueue = []
-
-function addNewPizza(pizzaObj) {
-    menu.push(pizzaObj)
-}
-
-function placeOrder(pizzaName) {
-    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
-    cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
-    orderQueue.push(newOrder)
-    return newOrder
-}
-
-function completeOrder(orderId) {
-    const order = orderQueue.find(order => order.id === orderId)
-    order.status = "completed"
-    return order
-}
-
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
-
-placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
-
-console.log("Menu:", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+let numberOfWheels: number = 4
+let isStudent: boolean = false
