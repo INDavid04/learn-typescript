7164346ec81b9d56a48f363b8b0268ed76036a3f
INDavid04 <davidirimia04@gmail.com>
2024-09-21 17:33:28 +0300

    add: l-0b Typing arrays

--- index.ts
+++ index.ts
@@ -1,59 +1,24 @@
-type Pizza = {
+type Person = {
     name: string
-    price: number
+    age: number
+    isStudent: boolean
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
 }
 
 /**
- * Challenge: Add an Order type. It should have `id`, `pizza`, and `status` properties.
- * Look through the code if you need a reminder as to what data types those should be.
+ * Challenge: create an array of people objects and
+ * manually type it as an array of Person types
  */
 
-type Order = {
-    id: number
-    pizza: Pizza
-    status: string
-}
-
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
-]
-
-let cashInRegister = 100
-let nextOrderId = 1
-const orderQueue = []
-
-function addNewPizza(pizzaObj: Pizza) {
-    menu.push(pizzaObj)
-}
-
-function placeOrder(pizzaName: string) {
-    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
-    if (!selectedPizza) {
-        console.error(`${pizzaName} does not exist in the menu`)
-        return
-    }
-    cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
-    orderQueue.push(newOrder)
-    return newOrder
-}
-
-function completeOrder(orderId: number) {
-    const order = orderQueue.find(order => order.id === orderId)
-    order.status = "completed"
-    return order
-}
-
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
-
-placeOrder("Chicken Bacon Ranch")
-completeOrder(1)
-
-console.log("Menu:", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+let people: Person[] = [person1, person2]
