41eec17fa6cb175d8e36e35a06d8621aa836e7fb
INDavid04 <davidirimia04@gmail.com>
2024-09-21 17:27:19 +0300

    add: l-0a Adding an Order type

--- index.ts
+++ index.ts
@@ -1,38 +1,59 @@
-// Challenge: try to figure out how to move the nested address object type
-// into a separate type definition. When done correctly, there should be no more
-// red errors in the editor
-
-type Address = {
-    street: string
-    city: string
-    country: string
-}
-
-type Person = {
+type Pizza = {
     name: string
-    age: number
-    isStudent: boolean
-    address: Address
+    price: number
 }
 
-let person1: Person = {
-    name: "Joe",
-    age: 42,
-    isStudent: true,
-    address: {
-        street: "123 Main",
-        city: "Anytown",
-        country: "USA"
-    }
+/**
+ * Challenge: Add an Order type. It should have `id`, `pizza`, and `status` properties.
+ * Look through the code if you need a reminder as to what data types those should be.
+ */
+
+type Order = {
+    id: number
+    pizza: Pizza
+    status: string
 }
 
-let person2: Person = {
-    name: "Jill",
-    age: 66,
-    isStudent: false,
-    address: {
-        street: "123 Main",
-        city: "Anytown",
-        country: "USA"
-    }
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+let cashInRegister = 100
+let nextOrderId = 1
+const orderQueue = []
+
+function addNewPizza(pizzaObj: Pizza) {
+    menu.push(pizzaObj)
 }
+
+function placeOrder(pizzaName: string) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    if (!selectedPizza) {
+        console.error(`${pizzaName} does not exist in the menu`)
+        return
+    }
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId: number) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder(1)
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
